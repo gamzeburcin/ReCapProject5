ae2e46e8db4aff81ff216ccab5ee37a3998fb03d
Your Name <you@example.com>
2021-02-23 15:50:42 +0300

    10.4.Ödev

---
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, RentACarContext>, IRentalDal
+    {
+        public List<RentalDetailDto> GetRentalDetails()
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                var result = from r in context.Rentals
+                             join c in context.Cars on r.CarId equals c.CarId
+                             join b in context.Brands on c.BrandId equals b.BrandId
+                             join cus in context.Customers on r.CustomerId equals cus.Id
+                             select new RentalDetailDto
+                             {
+                                 Id = r.Id,
+                                 RentDate = r.RentDate,
+                                 ReturnDate = r.ReturnDate,
+                                 CarName = b.BrandName,
+                                 CustomerName = cus.CompanyName
+                             };
+                return result.ToList();
+            }
+        }
+    }
+}
